cd04ccfb5a083c2fa5354b63dcd0a6872333edcb
pit135 <wermasaubunpetrus@gmail.com>
2025-09-16 11:13:27 +0700

    perbaiki csrf_t_o

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "petrus-wermasaubun-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://petrus-wermasaubun-footballnews.pbp.cs.ui.ac.id/"
+    "https://petrus-wermasaubun-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
